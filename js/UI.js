580226f40f557f4e431c74bea3dd5840fcbe5457
Dylan_Macnab <dylanmacnab@gmail.com>
2015-12-09 11:02:21 -0800

    Add roll dice funtion

--- js/UI.js
+++ js/UI.js
@@ -1,1 +1,12 @@
+// Helper function for print
+function printNumber(number) {
+  var placeholder = document.getElementById("placeholder");
+  placeholder.innerHTML = number;
+}
 
+var button = document.getElementById("button");
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
