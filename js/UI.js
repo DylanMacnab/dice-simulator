8f910245741aaf1043a32d32cb7891a59b5cd727
Dylan_Macnab <dylanmacnab@gmail.com>
2015-12-09 13:15:47 -0800

    Set number of sides on roll

--- js/UI.js
+++ js/UI.js
@@ -5,7 +5,23 @@ function printNumber(number) {
 }
 
 var button = document.getElementById("button");
+
 button.addEventListener("click", function(){
-  var result = dice.roll();
-  printNumber(result);
+  // Get Number of Sides
+  var sidesInput = document.getElementById("sidesInput").value;
+  dice.setSides(sidesInput);
+  // Set length of roll time to random value from 100 to 1000
+  var times = Math.floor(Math.random() * 10) + 1;
+  // Create Timer
+  var i = 0;
+  var timer = setInterval(function() {
+    if(i === times) {
+      clearInterval(timer);
+    }
+    // Roll the dice
+    var result = dice.roll();
+    printNumber(result);
+    // Incriment
+    i++;
+  }, 100);
 });
