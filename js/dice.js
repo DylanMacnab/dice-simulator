8bdc67bd2c62ec1bbb04d63d2c87a39909929a21
Dylan_Macnab <dylanmacnab@gmail.com>
2015-12-09 13:24:46 -0800

    Add setSides method to dice

--- js/dice.js
+++ js/dice.js
@@ -1,11 +1,15 @@
+
 // Dice Constructor Function
-function Dice(sides) {
-  this.sides = sides;
+function Dice() {
+  this.sides = 6;
+  this.setSides = function(sides) {
+    this.sides = sides;
+  };
   this.roll = function() {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     return randomNumber;
-  }
+  };
 }
 
 // Create an instance of Dice
-var dice = new Dice(50);
+var dice = new Dice();
